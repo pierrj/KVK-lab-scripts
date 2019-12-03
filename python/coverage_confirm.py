deb7e9acc7279347f4cf46df14a332da4d37f379
Pierre Joubert <pierrj@berkeley.edu>
2019-12-03 10:59:59 -0800

    fixed exception loophole

--- python/coverage_confirm.py
+++ python/coverage_confirm.py
@@ -1,6 +1,5 @@
 import csv
 import statistics
-from statistics import StatisticsError
 
 with open('parallel.confirmed') as confirmed:
     confirmed_reader = csv.reader(confirmed, delimiter = '\t')
@@ -16,45 +15,46 @@ for i in range(len(confirmed_list)):
     ecc = confirmed_list[i]
     region_len = ecc[2] - ecc[1]
     region = coverage_indexed[ecc[0]][ecc[1]:(ecc[2]+1)]
-    region_cov = [region[i][1] for i in range(len(region))]
-    mean_region = statistics.mean(region_cov)
+    region_cov = [region[k][1] for k in range(len(region))]
+    mean_region = round(statistics.mean(region_cov), 2)
+    beforestart = ecc[1] - 1 - region_len
+    afterstart = ecc[2] + 1
     region_before = coverage_indexed[ecc[0]][beforestart:beforestart + 1 + region_len]
-    region_before_cov = [region_before[i][1] for i in range(len(region_before))]
+    region_before_cov = [region_before[j][1] for j in range(len(region_before))]
     region_after = coverage_indexed[ecc[0]][afterstart:afterstart + 1 + region_len]
-    region_after_cov = [region_after[i][1] for i in range(len(region_after))]
-    try:
-        mean_before = statistics.mean(region_before_cov)
-    except StatisticsError:
+    region_after_cov = [region_after[g][1] for g in range(len(region_after))]
+    if beforestart > 0:
+        mean_before = round(statistics.mean(region_before_cov), 2)
+    else:
         mean_before = 'N/A'
-    try:
-        mean_after = statistics.mean(region_after_cov)
-    except StatisticsError:
+    if afterstart + region_len <= coverage_indexed[ecc[0]][-1][0]:
+        mean_after = round(statistics.mean(region_after_cov), 2)
+    else:
         mean_after = 'N/A'
+    coverage_string = str(mean_before)+';'+str(mean_region)+';'+str(mean_after)
     if region_cov.count(0) / len(region_cov) > 0.05:
         ecc.append('lowq')
         ecc.append('incomplete_coverage')
-        ecc.append(str(mean_before)+str(mean_region)+str(mean_after))
+        ecc.append(coverage_string)
         continue
-    beforestart = ecc[1] - 1 - region_len
     if beforestart <= 0:
         ecc.append('conf')
         ecc.append('too_close_to_start')
-        ecc.append(str(mean_before)+str(mean_region)+str(mean_after))
+        ecc.append(coverage_string)
         continue
-    afterstart = ecc[2] + 1
     if afterstart + region_len > coverage_indexed[ecc[0]][-1][0]:
         ecc.append('conf')
         ecc.append('too_close_to_end')
-        ecc.append(str(mean_before)+str(mean_region)+str(mean_after))
+        ecc.append(coverage_string)
         continue
     if mean_region >= (2*mean_before) and mean_region >= (2*mean_after):
         ecc.append('hconf')
         ecc.append('none')
-        ecc.append(str(mean_before)+str(mean_region)+str(mean_after))
+        ecc.append(coverage_string)
     else:
-        ecc.append('lconf')
+        ecc.append('conf')
         ecc.append('coverage_too_low')
-        ecc.append(str(mean_before)+str(mean_region)+str(mean_after))
+        ecc.append(coverage_string)
 
 with open('ecc_confidence', 'w', newline = '') as conf:
     w = csv.writer(conf, delimiter = '\t')
