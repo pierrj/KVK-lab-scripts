f6cb5a114b5ef2fee1ef800384a40babf70c4057
Pierre Joubert <pierrj@berkeley.edu>
2019-12-09 16:06:53 -0800

    added bash commands to go from lengthfiltered eccs to the bed file

--- python/getsrloc_eccconfirm.py
+++ python/getsrloc_eccconfirm.py
@@ -17,7 +17,25 @@ with open('samechromosome.exactlytwice.reverseread1.G3_1A_bwamem.bam', newline =
                 w.writerow(line1)
                 w.writerow(line2)
 
-with open('splitreads.sorted.reverseread1.G3_1A_bwamem.bed', newline = '') as file:
+filename = 'getsrloc_test'
+filtered_filename = 'qualityfiltered.' + filename
+exactlytwice_filename = 'exactlytwice.' + filtered_filename
+actualbam_filename = 'actualbam.' + exactlytwice_filename
+bedfile = filename + '.bed'
+samtools = '/global/home/groups/consultsw/sl-7.x86_64/modules/samtools/1.8/bin/samtools'
+bedtools = '/global/home/groups/consultsw/sl-7.x86_64/modules/bedtools/2.28.0/bin/bedtools'
+
+quality_filter = '''awk '{a=gensub(/^([0-9]+)M.*[HS]$/, "\\\\1", "", $6); b=gensub(/.*[HS]([0-9]+)M$/, "\\\\1", "", $6); if((a !~ /[DMIHS]/ && int(a) > 49 ) || (b !~ /[DMIHS]/ && int(b) > 49)) print $0}' ''' + filename + ''' > ''' + filtered_filename
+exactlytwice_filter = '''awk 'NR==FNR{a[$1, $3]++; next} a[$1, $3]==2' ''' + filtered_filename + ''' ''' + filtered_filename + ''' > ''' + exactlytwice_filename
+make_bam = 'bash -c \"' + samtools + ' view -b -h <(cat <(' + samtools + ' view -H G3_1A_bwamem.bam) ' + exactlytwice_filename +') > ' + actualbam_filename + '\"'
+bamtobed_sort = bedtools + ' bamtobed -i ' + actualbam_filename + ' | sort -k 4,4 -k 2,2 > ' + bedfile
+
+subprocess.run(quality_filter, shell= True)
+subprocess.run(exactlytwice_filter, shell= True)
+subprocess.run(make_bam, shell= True)
+subprocess.run(bamtobed_sort, shell= True)
+
+with open('getsrloc_test.bed', newline = '') as file:
     file_reader = csv.reader(file, delimiter = '\t')
     with open('merged.getsrloc_test', 'w', newline = '') as filtered:
         w = csv.writer(filtered, delimiter = '\t')
