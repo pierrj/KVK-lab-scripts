643f7ca7bb706ac160f032b5e0e8702d3e91511e
Pierre Joubert <pierrj@berkeley.edu>
2020-03-07 16:35:08 -0800

    switch to v2

---
+++ bash/ecc_callerv2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ls *.fastq | awk '{print substr($0,0,5)}' | sort | uniq > mapfile
+while read sample;
+do
+    sbatch --job-name=$sample.ecc_caller --export=sample=$sample /global/home/users/pierrj/git/slurm/ecc_callerv2.slurm
+done < mapfile
