e2f4457595b5c366a15d4b866c3ed8c0034acccc
Pierre Joubert <pierrj@berkeley.edu>
2020-04-02 12:03:53 -0700

    now get spikein SRs for each sample

--- bash/test_getSRcount.sh
+++ bash/test_getSRcount.sh
@@ -2,4 +2,4 @@
 pgbt9=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pGBT9)
 puc19=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pUC19_tweaked)
 pbr322=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pBR322_tweaked)
-echo -e ${1}'\t'$pgbt9'\t'$puc19'\t'$pbr322 # >> /global/scratch/users/pierrj/eccDNA/pipeline_tests/normalization/readcounts
+echo -e ${1}'\t'$pgbt9'\t'$puc19'\t'$pbr322 >> /global/scratch/users/pierrj/eccDNA/pipeline_tests/normalization/SR_counts
