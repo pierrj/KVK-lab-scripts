30328c5e3ed68ab3066e1fa4df900817003be6af
Pierre Joubert <pierrj@berkeley.edu>
2020-04-01 18:54:02 -0700

    try with tweaked spike ins

---
+++ bash/test_getSRcount.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pgbt9=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pGBT9)
+puc19=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pUC19_tweaked)
+pbr322=$(/global/home/users/pierrj/git/bash/get_SR_count.sh ${1} pBR322_tweaked)
+echo -e ${1}'\t'$pgbt9'\t'$puc19'\t'$pbr322 # >> /global/scratch/users/pierrj/eccDNA/pipeline_tests/normalization/readcounts
