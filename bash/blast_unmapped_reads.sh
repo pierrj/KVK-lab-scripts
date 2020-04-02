fb2b62c33387804c73473c4fd6f5099c2f6bb6d1
Pierre Joubert <pierrj@berkeley.edu>
2020-04-02 15:38:01 -0700

    pushing some changes

--- bash/blast_unmapped_reads.sh
+++ bash/blast_unmapped_reads.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #ls *.fastq | awk '{print substr($0,0,5)}' | sort | uniq > mapfile
 while read sample;
-if [ ${sample:0:2} = "IF" ]
 do
-    sbatch --job-name=$sample.unmapped_reads --export=sample=$sample /global/home/users/pierrj/git/slurm/blast_unmapped_reads_loop.slurm
+    if [ ${sample:0:2} = "IF" ]
+        then sbatch --job-name=$sample.unmapped_reads --export=sample=$sample /global/home/users/pierrj/git/slurm/blast_unmapped_reads_loop.slurm
+    fi
 done < mapfile
