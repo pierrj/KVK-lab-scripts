22c69e01cc9f1aa7fffa6c077873c5886b84dbd2
Pierre Joubert <pierrj@berkeley.edu>
2020-04-04 14:56:28 -0700

    dont need ls anymore

--- bash/ecc_callerv3_pureculture.sh
+++ bash/ecc_callerv3_pureculture.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ls *.fastq | awk '{print substr($0,0,5)}' | sort | uniq > mapfile
+# ls *.fastq | awk '{print substr($0,0,5)}' | sort | uniq > mapfile
 while read sample;
 do
     sbatch --job-name=$sample.ecc_caller --export=sample=$sample /global/home/users/pierrj/git/slurm/ecc_callerv3_pureculture.slurm
