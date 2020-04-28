c46f323519ed6154714d2e105c314769967f9247
Pierre Joubert <pierrj@berkeley.edu>
2020-04-28 12:33:54 -0700

    oops args were wrong

--- R/histogram.R
+++ R/histogram.R
@@ -1,6 +1,6 @@
 args = commandArgs(trailingOnly=TRUE)
 
 chol <- read.table(args[1], header = FALSE)
-tiff(args[1], units="in", width=5, height=5, res=300)
+tiff(args[2], units="in", width=5, height=5, res=300)
 hist(chol$V1, breaks=1000)
 dev.off()
