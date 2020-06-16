ef230ddbb2c2302aa5ff199e9a39422b702182fd
Pierre Joubert <pierrj@berkeley.edu>
2020-06-16 15:35:33 -0700

    ylim isnt working for some reason

--- R/make_coverageplot.R
+++ R/make_coverageplot.R
@@ -4,5 +4,5 @@ args = commandArgs(trailingOnly=TRUE)
 
 my_data <- read.delim(args[1])
 tiff(args[2], units="in", width=25, height=5, res=300)
-manhattan(my_data, chr = "CHROMOSOME", bp = "BASE", snp = "BASE", p = "COUNT", logp=FALSE, ylab= NA , xlab= NA , genomewideline=FALSE, suggestiveline = FALSE, main = NA, annotateTop = TRUE, cex.lab=1.5, cex.axis = 1.5, ylim = c(0,args[3]), col = c("blue4", "orange3"))
+manhattan(my_data, chr = "CHROMOSOME", bp = "BASE", snp = "BASE", p = "COUNT", logp=FALSE, ylab= NA , xlab= NA , genomewideline=FALSE, suggestiveline = FALSE, main = NA, annotateTop = TRUE, cex.lab=1.5, cex.axis = 1.5, ylim = c(0,strtoi(args[3])), col = c("blue4", "orange3"))
 dev.off()
