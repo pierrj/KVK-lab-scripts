060900333638a137ba31051abf838700f7374739
Pierre Joubert <pierrj@berkeley.edu>
2020-06-16 17:08:04 -0700

    fix pid file name

--- python/ecc_caller_anygenome.py
+++ python/ecc_caller_anygenome.py
@@ -104,7 +104,7 @@ def confirmeccs(ecc):
     return False
 
 # open parallelization client
-rc = ipp.Client(profile='default', cluster_id = "slurm-" + os.environ['SLURM_JOBID'])
+rc = ipp.Client(profile='default', cluster_id = "cluster-id-" + output_name)
 dview = rc[:]
 dview.block = True
 lview = rc.load_balanced_view()
