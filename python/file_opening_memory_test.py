c547fb0ecd09fddce2a0426db001b063b5f108e7
Pierre Joubert <pierrj@berkeley.edu>
2020-06-26 10:07:46 -0700

    okay now with pushing to one cluster

--- python/file_opening_memory_test.py
+++ python/file_opening_memory_test.py
@@ -31,3 +31,31 @@ with open(outwardfacing_read_file, newline = '') as discordant:
         discordant_indexed[(int(row[0])-1)].append([int(row[1]), int(row[2])])
 
 print('successfully opened files')
+
+def confirmeccs(ecc):
+    for i in range(0, len(discordant_indexed[ecc[0]]), 2):
+        reada = discordant_indexed[ecc[0]][i]
+        readb = discordant_indexed[ecc[0]][i+1]
+        # on the fly sorting because the discordant reads are next to each other but not sorted by base position
+        if reada[0] < readb[0]:
+            read1 = reada
+            read2 = readb
+        else:
+            read1 = readb
+            read2 = reada
+        if ecc[1] <= read1[0] <= ecc[2] and ecc[1] <= read1[1] <= ecc[2] and ecc[1] <= read2[0] <= ecc[2] and ecc[1] <= read2[1] <= ecc[2]:
+            # calculate total distance from ecc start and end
+            distance = abs(read1[1] - ecc[1]) + abs(read2[0] - ecc[2])
+            # set here for insert size distribution
+            if distance <= 500:
+                return True
+    return False
+
+rc = ipp.Client(profile='default', cluster_id = "cluster-id-" + output_name)
+dview = rc[:]
+dview.block = True
+lview = rc.load_balanced_view()
+lview.block = True
+
+mydict = dict(discordant_indexed = discordant_indexed)
+dview.push(mydict)
