4196359dbf945a6637841f5da4611d70b2834ef9
Pierre Joubert <pierrj@berkeley.edu>
2020-07-09 10:31:45 -0700

    okay now pushing list 2

--- python/dummy_ipython_parallel.py
+++ python/dummy_ipython_parallel.py
@@ -6,24 +6,24 @@ list2 = [ [i, i, i] for i in range(2000000, 6000000)]
 
 print('generated list')
 
-# def loop(item):
-#     for i in range(len(list2)):
-#         if list2[i][0] == item[0]:
-#             return True
-#     return False
+def loop(item):
+    for i in range(len(list2)):
+        if list2[i][0] == item[0]:
+            return True
+    return False
 
-# rc = ipp.Client(profile='default', cluster_id = "cluster-id-dummy")
-# dview = rc[:]
-# dview.block = True
-# lview = rc.load_balanced_view()
-# lview.block = True
+rc = ipp.Client(profile='default', cluster_id = "cluster-id-dummy")
+dview = rc[:]
+dview.block = True
+lview = rc.load_balanced_view()
+lview.block = True
 
-# print('connected to engines')
+print('connected to engines')
 
-# mydict = dict(list2 = list2)
-# dview.push(mydict)
+mydict = dict(list2 = list2)
+dview.push(mydict)
 
-# print('pushed list 2')
+print('pushed list 2')
 
 # trueorfalse = list(lview.map(loop, list1))
 
