e51c17936a64d05d4b325d242d759a6c0bbfc32c
Pierre Joubert <pierrj@berkeley.edu>
2020-07-10 16:18:55 -0700

    with writing output

--- python/gnu_parallel_test.py
+++ python/gnu_parallel_test.py
@@ -4,5 +4,12 @@ import csv
 test_file = str(sys.argv[1])
 
 with open(test_file, newline = '') as file:
+    list1 = []
     for row in file_reader:
-        print(row)
+        list1.append(row)
+
+output = 'processed' + test_file
+
+with open(output, 'w', newline = '') as out:
+    w = csv.writer(out, delimiter = '\t')
+    w.writerows(out)
