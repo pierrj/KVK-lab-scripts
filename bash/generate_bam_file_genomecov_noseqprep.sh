6ee1a1fb3c9eb7f3ab7c5c08bee2b4f8e6db1e3c
Pierre Joubert <pierrj@berkeley.edu>
2020-07-22 14:22:12 -0700

    noseqprep run of ossr

---
+++ bash/generate_bam_file_genomecov_noseqprep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+while getopts g:1:2:s:t: option
+do
+case "${option}"
+in
+g) GENOME_DB=${OPTARG};;
+1) READONE=${OPTARG};;
+2) READTWO=${OPTARG};;
+s) SAMPLE=${OPTARG};;
+t) THREADS=${OPTARG};;
+esac
+done
+
+gzip -c ${READONE} > tmp.seqprep.${SAMPLE}_R1.fastq.gz
+gzip -c ${READTWO} > tmp.seqprep.${SAMPLE}_R2.fastq.gz
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --nextseq-trim=20 -o tmp.trimmed.seqprep.${SAMPLE}_R1.fastq -p tmp.trimmed.seqprep.${SAMPLE}_R2.fastq tmp.seqprep.${SAMPLE}_R1.fastq.gz tmp.seqprep.${SAMPLE}_R2.fastq.gz
+bwa mem -t ${THREADS} ${GENOME_DB} tmp.trimmed.seqprep.${SAMPLE}_R1.fastq tmp.trimmed.seqprep.${SAMPLE}_R2.fastq -o tmp.seqprep.trimmed.${SAMPLE}_bwamem.sam
+samtools view -S -b tmp.seqprep.trimmed.${SAMPLE}_bwamem.sam > ${SAMPLE}.mergedandpe.bwamem.bam
+samtools sort ${SAMPLE}.mergedandpe.bwamem.bam > ${SAMPLE}.sorted.mergedandpe.bwamem.bam
+samtools index ${SAMPLE}.sorted.mergedandpe.bwamem.bam
+
+bedtools genomecov -d -ibam ${SAMPLE}.sorted.mergedandpe.bwamem.bam > ${SAMPLE}.genomecoverage.bed
+
+rm tmp*
