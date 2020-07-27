b5350e9b35f7c428608e348c6c06651ee0dd4376
Pierre Joubert <pierrj@berkeley.edu>
2020-07-27 12:37:30 -0700

    just added some new scripts to easily average signal across biological and technical replicates

---
+++ bash/create_mapfile_for_normalize_and_average.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+while getopts m:t:n: option ### THIS NEEDS TO BE FIXED ####
+do
+case "${option}"
+in
+m) MAPFILE=${OPTARG}
+t) FILE_EXTENSION=${OPTARG}
+n) NORMALIZE_EXTENSION=${OPTARG}
+esac
+done
+
+if [ -f "mapfile_for_normalize_and_average" ]; then
+    rm mapfile_for_normalize_and_average
+fi
+
+filtered.sorted.RC_1A.bam
+
+while read line; 
+do
+    sample=$(echo "$line" | cut -f1)
+    bio_rep=$(echo "$line" | cut -f2)
+    treatment=$(echo "$line" | cut -f3)
+    cd ${sample}
+    file_path=$(realpath ${sample}${FILE_EXTENSION})
+    normalize_path=$(realpath ${NORMALIZE_EXTENSION}${sample}.bam)
+    cd ..
+    echo -e ${file_path}'\t' ${normalize_path}'\t'${bio_rep}'\t'${treatment} >> mapfile_for_normalize_and_average
+done < ${MAPFILE}
---
+++ bash/normalize_and_average.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+while getopts m:f:b: option
+do
+case "${option}"
+in
+m) MAPFILE=${OPTARG}
+f) SCALING_FACTOR=${OPTARG};; ## for scaling data so numbers arent so gross, usually 1000000
+b) BIN_SIZE=${OPTARG};; ## this is for averaging bins to a certain size, should be optional as well, usually 100
+esac
+done
+
+while read line; 
+do
+    target_file=$(echo "$line" | cut -f1)
+    normalize_file=$(echo "$line" | cut -f2)
+    bio_rep=$(echo "$line" | cut -f3)
+    treatment=$(echo "$line" | cut -f4)
+    normalize_factor=$(samtools view -c -F 4 -F 2048 ${normalize_file} | awk -v S=${SCALING_FACTOR} '{print $1/S' )
+    echo -e ${bio_rep} '\t' ${treatment} >> bio_rep_mapfile
+    echo ${treatment} >> treatment_mapfile
+    awk -v N=${normalize_factor} -v B=${BIN_SIZE} '{sum+=$3} NR%B==0 {print sum/B/N; sum =0}' ${target_file} > ${target_file}.normalized_binned.${bio_rep}.${treatment}
+done < ${MAPFILE}
+
+while read line; 
+do
+    bio_rep=$(echo "$line" | cut -f1)
+    treatment=$(echo "$line" | cut -f2)
+    paste $(find . -maxdepth 1 -name "*normalized_binned.${bio_rep}*" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > ${bio_rep}.normalized_binned.${treatment}
+done < bio_rep_mapfile
+
+while read treatment; 
+do
+    paste $(find . -maxdepth 1 -name "*.normalized_binned.${treatment}" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > ${treatment}.normalized_binned
+done < treatment_mapfile
+
+## clean up
+while read line; 
+do
+    target_file=$(echo "$line" | cut -f1)
+    normalize_file=$(echo "$line" | cut -f2)
+    bio_rep=$(echo "$line" | cut -f3)
+    treatment=$(echo "$line" | cut -f4)
+    mv ${target_file}.normalized_binned.${tech_rep}.${bio_rep}.${treatment} ${target_file}.normalized_binned
+done < mapfile
+
+while read line; 
+do
+    bio_rep=$(echo "$line" | cut -f1)
+    treatment=$(echo "$line" | cut -f2)
+    mv ${bio_rep}.normalized_binned.${treatment} ${bio_rep}.normalized_binned
+done < bio_rep_mapfile
+
+rm bio_rep_mapfile
+rm treatment_mapfile
