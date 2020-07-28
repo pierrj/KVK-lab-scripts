494354b1ed2eb02516c18dbc54ce25ab3a9eec07
Pierre Joubert <pierrj@berkeley.edu>
2020-07-28 15:13:41 -0700

    this should add columns so that the input fits with the coverage plot scripts

--- bash/normalize_and_average.sh
+++ bash/normalize_and_average.sh
@@ -17,41 +17,28 @@ do
     bio_rep=$(echo "$line" | cut -f4)
     treatment=$(echo "$line" | cut -f5)
     normalize_factor=$(samtools view -c -F 4 -F 2048 ${normalize_file} | awk -v S=${SCALING_FACTOR} '{print $1/S}' )
-    awk -v N=${normalize_factor} -v B=${BIN_SIZE} '{sum+=$3} NR%B==0 {print sum/B/N; sum =0}' ${target_file} > ${sample}.normalized_binned.${bio_rep}.${treatment}
+    awk -v N=${normalize_factor} -v B=${BIN_SIZE} -v OFS='\t' '{sum+=$3} NR%B==0 {print $1, $2, sum/B/N; sum =0}' ${target_file} > ${sample}.normalized_binned
+    cut -f3 ${sample}.normalized_binned > tmp_normalize_and_average.${sample}.normalized_binned.${bio_rep}.${treatment}
 done < ${MAPFILE}
 
-while read line; 
-do
-    bio_rep=$(echo "$line" | cut -f4)
-    treatment=$(echo "$line" | cut -f5)
-    paste $(find . -maxdepth 1 -name "*normalized_binned.${bio_rep}*" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > ${bio_rep}.normalized_binned.${treatment}
-done < ${MAPFILE}
-
-while read line; 
-do
-    treatment=$(echo "$line" | cut -f5)
-    paste $(find . -maxdepth 1 -name "*.normalized_binned.${treatment}" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > ${treatment}.normalized_binned
-done < ${MAPFILE}
-
-## clean up
-
-while read line; 
-do
-    target_file=$(echo "$line" | cut -f1)
-    normalize_file=$(echo "$line" | cut -f2)
-    sample=$(echo "$line" | cut -f3)
-    bio_rep=$(echo "$line" | cut -f4)
-    treatment=$(echo "$line" | cut -f5)
-    mv ${sample}.normalized_binned.${bio_rep}.${treatment} ${sample}.normalized_binned
-done < ${MAPFILE}
-
-cut -f4- ${MAPFILE} | sort | uniq > bio_rep_mapfile
+awk -v OFS='\t' '{print $1, $2}' ${sample}.normalized_binned > tmp_normalize_and_average_first_two_columns ## THIS LINE MEANS ALL OF THE FILES IN A RUN SHOULD BE MAPPED TO THE SAME GENOME
+cut -f4- ${MAPFILE} | sort | uniq > tmp_normalize_and_average_bio_rep_mapfile
 
 while read line; 
 do
     bio_rep=$(echo "$line" | cut -f1)
     treatment=$(echo "$line" | cut -f2)
-    mv ${bio_rep}.normalized_binned.${treatment} ${bio_rep}.normalized_binned
-done < bio_rep_mapfile
+    paste $(find . -maxdepth 1 -name "*normalized_binned.${bio_rep}*" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > tmp_normalize_and_average.${bio_rep}.normalized_binned.${treatment}
+    paste tmp_normalize_and_average_first_two_columns tmp_normalize_and_average.${bio_rep}.normalized_binned.${treatment} > ${bio_rep}.normalized_binned 
+done < tmp_normalize_and_average_bio_rep_mapfile
 
-rm bio_rep_mapfile
+cut -f5- ${MAPFILE} | sort | uniq > tmp_normalize_and_average_treatment_mapfile
+
+while read line; 
+do
+    treatment=$(echo "$line")
+    paste $(find . -maxdepth 1 -name "*.normalized_binned.${treatment}" | xargs -r ls -1 | cut -c 3- | tr "\n" " ") | awk '{sum = 0; for (i = 1; i <= NF; i++) sum += $i; sum /= NF; print sum}' > tmp_normalize_and_average.${treatment}.normalized_binned
+    paste tmp_normalize_and_average_first_two_columns tmp_normalize_and_average.${treatment}.normalized_binned > ${treatment}.normalized_binned 
+done < tmp_normalize_and_average_treatment_mapfile
+
+rm tmp_normalize_and_average*
