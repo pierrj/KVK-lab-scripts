cde1285e4fa98ab99076d9fa9925e36a817124e9
Pierre Joubert <pierrj@berkeley.edu>
2020-08-18 09:57:38 -0700

    a bunch of bugs

--- bash/get_normalization_factor.sh
+++ bash/get_normalization_factor.sh
@@ -11,16 +11,16 @@ o) SLOPE_FILE=${OPTARG};;
 esac
 done
 
+if [ -f "spike_normalization_countable_${SAMPLE}" ]; then
+    rm spike_normalization_countable_${SAMPLE}
+fi
 ## input is a table with the ratio in column one and the spike in name as listed in the mapped fasta file in column two
 while read ratio spike
 do
 echo ${spike} > tmp.mapfile_${spike}
 samtools view -b -q 1 ${BAMFILE} $(cat tmp.mapfile_${spike} | tr "\n" " ") > tmp.filtered.sorted.${SAMPLE}_${spike}.bam
-/global/home/users/pierrj/git/bash/call_ecc_regions.sh -m tmp.mapfile_spikeone \
-    -s ${SAMPLE}_${spike} -t ${THREADS} -b tmp.filtered.sorted.uniquemap.${SAMPLE}.bam
-if [ -f "spike_normalization_countable_${SAMPLE}" ]; then
-    rm spike_normalization_countable_${SAMPLE}
-fi
+/global/home/users/pierrj/git/bash/call_ecc_regions.sh -m tmp.mapfile_${spike} \
+    -s ${SAMPLE}_${spike} -t ${THREADS} -b tmp.filtered.sorted.${SAMPLE}_${spike}.bam
 count=$(wc -l ${SAMPLE}_${spike}.confirmedsplitreads.bed)
 echo -e ${ratio}'\t'${spike}'\t'${count} >> spike_normalization_countable_${SAMPLE}
 done < ${SPIKE_MAPFILE}
