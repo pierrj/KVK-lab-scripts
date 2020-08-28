4d2f93479a9224d1a6ab1feb91474180e9b26cfa
Pierre Joubert <pierrj@berkeley.edu>
2020-08-28 11:26:19 -0700

    adding scripts to output GO results for input files

---
+++ R/go_enrichment.R
@@ -0,0 +1,28 @@
+library(topGO)
+
+args = commandArgs(trailingOnly=TRUE)
+
+geneID2GO <-  readMappings(args[1])
+
+geneNames <- names(geneID2GO)
+
+myInterestingGenes <- as.character(read.csv(args[2],header=F, stringsAsFactors=FALSE, colClasses="character")$V1)
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes))
+tail(geneList)
+names(geneList) <- geneNames
+str(geneList)
+
+GOdata <- new("topGOdata", ontology = args[3], allGenes = geneList, annot = annFUN.gene2GO, gene2GO = geneID2GO)
+
+GOdata
+
+test.stat <- new("classicCount", testStatistic = GOFisherTest, name = "Fisher test")
+resultFisher <- getSigGroups(GOdata, test.stat)
+
+pvalFisher <- score(resultFisher)
+head(pvalFisher)
+
+allRes <- GenTable(GOdata, classic = resultFisher, orderBy = "weight", ranksOf = "classic", topNodes=20)
+
+
+write.table(allRes, file = args[4], sep = '\t', quote = FALSE)
---
+++ R/make_wordcloud.R
@@ -0,0 +1,14 @@
+library(wordcloud2)
+library(data.table)
+library(htmlwidgets)
+
+args = commandArgs(trailingOnly=TRUE)
+
+df <- data.frame(fread(args[1]))
+colnames(df) <- c("word", "freq")
+
+rownames(df) <- df[,1]
+
+mywordcloud = wordcloud2(data = df, size = 1)
+saveWidget(mywordcloud, "test1.html",selfcontained = F)
+webshot::webshot(args[2], args[2], vwidth = 1992, vheight = 1744, delay =10)
