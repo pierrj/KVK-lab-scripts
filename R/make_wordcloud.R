548d34022047af95158e06ceee1eb13969acd868
Pierre Joubert <pierrj@berkeley.edu>
2020-09-25 13:21:11 -0700

    webshot needs to open the html file and take a screenshot

--- R/make_wordcloud.R
+++ R/make_wordcloud.R
@@ -10,5 +10,5 @@ colnames(df) <- c("word", "freq")
 rownames(df) <- df[,1]
 
 mywordcloud = wordcloud2(data = df, size = 1)
-saveWidget(mywordcloud, "test1.html",selfcontained = F)
-webshot::webshot(args[2], args[2], vwidth = 1992, vheight = 1744, delay =10)
+saveWidget(mywordcloud, "wordcloud.html",selfcontained = F)
+webshot::webshot("wordcloud.html", args[2], vwidth = 1992, vheight = 1744, delay =10)
