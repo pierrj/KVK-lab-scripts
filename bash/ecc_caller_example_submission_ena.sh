bee5d1fdb5f23bd639f34ba3f99ebe30f8d66fb8
Pierre Joubert <pierrj@berkeley.edu>
2020-10-15 13:22:32 -0700

    forgot to update slurm file

--- bash/ecc_caller_example_submission_ena.sh
+++ bash/ecc_caller_example_submission_ena.sh
@@ -9,7 +9,7 @@ genome_bwa="/global/scratch/users/pierrj/references/ORSA_w_organelles_bwa"
 sample="ERR1830502"
 READONE_LINK="ftp.sra.ebi.ac.uk/vol1/fastq/ERR183/002/ERR1830502/ERR1830502_1.fastq.gz"
 READTWO_LINK="ftp.sra.ebi.ac.uk/vol1/fastq/ERR183/002/ERR1830502/ERR1830502_2.fastq.gz"
-sbatch --job-name=$sample.ecc_caller_ena --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa /global/home/users/pierrj/git/slurm/ecc_caller_ena.slurm; done < mapfile_ena_ora
+sbatch --job-name=$sample.ecc_caller_ena --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa,READONE_LINK=$READONE_LINK,READTWO_LINK=$READTWO_LINK /global/home/users/pierrj/git/slurm/ecc_caller_ena.slurm
 
 while IFS=$"\t" read -r sample READONE_LINK READTWO_LINK
 do
@@ -19,3 +19,11 @@ echo $READTWO_LINK
 done < mapfile_ENA_ory
 
 while IFS=$'\t' read -r sample READONE_LINK READTWO_LINK; do echo ${sample}; echo $READONE_LINK; echo $READTWO_LINK; done < mapfile_ENA_ory
+
+mapfile="/global/scratch/users/pierrj/references/ORSA_no_organelles.contignames"
+genome_bwa="/global/scratch/users/pierrj/references/ORSA_w_organelles_bwa"
+while IFS=$'\t' read -r sample READONE_LINK READTWO_LINK; do sbatch --job-name=$sample.ecc_caller_ena --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa,READONE_LINK=$READONE_LINK,READTWO_LINK=$READTWO_LINK /global/home/users/pierrj/git/slurm/ecc_caller_ena.slurm; done < mapfile_ENA_ory
+
+mapfile="/global/scratch/users/pierrj/references/TAIR10.contignames"
+genome_bwa="/global/scratch/users/pierrj/references/TAIR10_bwa"
+while IFS=$'\t' read -r sample READONE_LINK READTWO_LINK; do sbatch --job-name=$sample.ecc_caller_ena --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa,READONE_LINK=$READONE_LINK,READTWO_LINK=$READTWO_LINK /global/home/users/pierrj/git/slurm/ecc_caller_ena.slurm; done < mapfile_ENA_ara
