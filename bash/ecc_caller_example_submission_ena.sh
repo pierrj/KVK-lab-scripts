d55387e990358eee882b07c19749ee7a0b857a6c
Pierre Joubert <pierrj@berkeley.edu>
2020-10-15 12:44:50 -0700

    adding ena ecc_caller

---
+++ bash/ecc_caller_example_submission_ena.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mapfile="/global/scratch/users/pierrj/eccDNA/2018_moller/references/GCF_000001405.25_GRCh37.p13_genomic.contignames"
+genome_bwa="/global/scratch/users/pierrj/eccDNA/2018_moller/references/GRCh37.p13_bwa"
+while read sample; do sbatch --job-name=$sample.ecc_caller_sra --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa /global/home/users/pierrj/git/slurm/ecc_caller_sra.slurm; done < mapfile
+
+
+mapfile="/global/scratch/users/pierrj/references/ORSA_no_organelles.contignames"
+genome_bwa="/global/scratch/users/pierrj/references/ORSA_w_organelles_bwa"
+sample="ERR1830502"
+READONE_LINK="ftp.sra.ebi.ac.uk/vol1/fastq/ERR183/002/ERR1830502/ERR1830502_1.fastq.gz"
+READTWO_LINK="ftp.sra.ebi.ac.uk/vol1/fastq/ERR183/002/ERR1830502/ERR1830502_2.fastq.gz"
+sbatch --job-name=$sample.ecc_caller_ena --export=sample=$sample,mapfile=$mapfile,genome_bwa=$genome_bwa /global/home/users/pierrj/git/slurm/ecc_caller_ena.slurm; done < mapfile_ena_ora
+
+while IFS=$"\t" read -r sample READONE_LINK READTWO_LINK
+do
+echo ${sample}
+echo $READONE_LINK
+echo $READTWO_LINK
+done < mapfile_ENA_ory
+
+while IFS=$'\t' read -r sample READONE_LINK READTWO_LINK; do echo ${sample}; echo $READONE_LINK; echo $READTWO_LINK; done < mapfile_ENA_ory
