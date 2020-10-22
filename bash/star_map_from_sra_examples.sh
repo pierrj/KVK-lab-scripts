a4d0daba7273abda8bd09dba3678ee12bc4eb7f5
Pierre Joubert <pierrj@berkeley.edu>
2020-10-22 12:05:16 -0700

    not going to use cutadapt anymore

--- bash/star_map_from_sra_examples.sh
+++ bash/star_map_from_sra_examples.sh
@@ -4,3 +4,8 @@ ADAPTER_TWO=AGATCGGAAGAGCGTCGTGTAGGGA
 sample=SRR7642295
 GENOME_DB=/global/scratch/users/pierrj/references/guy11_genome_baoetal2017_star
 sbatch --job-name=$sample.starmap --export=sample=$sample,GENOME_DB=$GENOME_DB,ADAPTER_ONE=$ADAPTER_ONE,ADAPTER_TWO=$ADAPTER_TWO /global/home/users/pierrj/git/slurm/star_map_from_sra_pe.slurm
+
+
+sample=SRR7642295
+GENOME_DB=/global/scratch/users/pierrj/references/guy11_genome_baoetal2017_star
+sbatch --job-name=$sample.starmap --export=sample=$sample,GENOME_DB=$GENOME_DB /global/home/users/pierrj/git/slurm/star_map_from_sra_pe.slurm
--- bash/star_map_from_sra_pe.sh
+++ bash/star_map_from_sra_pe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts g:1:2:s:a:b:t: option
+while getopts g:1:2:s:t: option
 do
 case "${option}"
 in
@@ -7,15 +7,12 @@ g) GENOME_DB=${OPTARG};;
 1) READONE=${OPTARG};;
 2) READTWO=${OPTARG};;
 s) SAMPLE=${OPTARG};;
-a) ADAPTER_ONE=${OPTARG};;
-b) ADAPTER_TWO=${OPTARG};;
 t) THREADS=${OPTARG};;
 esac
 done
 
 /global/home/users/pierrj/scripts/sratoolkit.2.10.4-centos_linux64/bin/prefetch ${SAMPLE} -O .
 /global/home/users/pierrj/scripts/sratoolkit.2.10.4-centos_linux64/bin/fasterq-dump -e ${THREADS} -O . -t tmp ${SAMPLE}.sra
-cutadapt -a ${ADAPTER_ONE} -A ${ADAPTER_TWO} -o tmp.trimmed.seqprep.${SAMPLE}_R1.fastq -p tmp.trimmed.seqprep.${SAMPLE}_R2.fastq ${READONE} ${READTWO}
 STAR --runThreadN ${THREADS} \
     --genomeDir ${GENOME_DB} \
-    --readFilesIn tmp.trimmed.seqprep.${SAMPLE}_R1.fastq tmp.trimmed.seqprep.${SAMPLE}_R2.fastq
+    --readFilesIn ${READONE} ${READTWO}
