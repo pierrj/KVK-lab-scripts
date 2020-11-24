2596ef7b3991172a740cde349d3e52a01d28c4fc
Pierre Joubert <pierrj@berkeley.edu>
2020-11-24 14:14:03 -0700

    updated my fungap, need to test but should run fine now

--- bash/my_run_fungap.sh
+++ bash/my_run_fungap.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 cd $1
-nohup python /Users/pierrj/fungap_local/FunGAP/fungap_patched.py \
+genome=$(basename $1)
+echo start >> /Users/pierrj/fungap_runs/moryzae_genomes/annotation_run_tracker/${genome}_run_tracker
+date +"%T" >> /Users/pierrj/fungap_runs/moryzae_genomes/annotation_run_tracker/${genome}_run_tracker
+
+python /Users/pierrj/fungap_local/FunGAP/fungap_patched.py \
   --output_dir fungap_out \
   --trans_read_1 SRR8842990_1.fastq \
   --trans_read_2 SRR8842990_2.fastq \
-  --genome_assembly guy11_genome_baoetal2017.fasta  \
+  --genome_assembly ${genome}.fasta  \
   --augustus_species magnaporthe_grisea  \
   --sister_proteome prot_db.faa  \
   --busco_dataset sordariomycetes_odb10 \
-  --num_cores 2 >& run.out &
+  --num_cores 1 >& run.out
+
+if [ ! -f ${1}/fungap_out/fungap_out/fungap_out.gff3 ]; then
+    echo $1 >> /Users/pierrj/fungap_runs/moryzae_genomes/annotation_run_tracker/did_not_complete
+fi
+
+echo end >> /Users/pierrj/fungap_runs/moryzae_genomes/annotation_run_tracker/${genome}_run_tracker
+date +"%T" >> /Users/pierrj/fungap_runs/moryzae_genomes/annotation_run_tracker/${genome}_run_tracker
