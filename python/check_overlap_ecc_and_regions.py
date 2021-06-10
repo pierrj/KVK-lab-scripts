38b56adc3dc41a99e17708f1d503b6d6f814cb98
Pierre Joubert <pierrj@berkeley.edu>
2021-06-10 11:02:11 -0700

    should be rtol on both

--- python/check_overlap_ecc_and_regions.py
+++ python/check_overlap_ecc_and_regions.py
@@ -32,7 +32,7 @@ for region in regions:
     start_region = region[1]
     end_region= region[2]
     eccs_for_scaffold = eccs_arrays[int(region[0][10:12])-1]
-    ecc_matches = eccs_for_scaffold[np.logical_and(np.isclose((eccs_for_scaffold[:,1]).astype(int), start_region, atol=tolerance),
+    ecc_matches = eccs_for_scaffold[np.logical_and(np.isclose((eccs_for_scaffold[:,1]).astype(int), start_region, atol=tolerance, rtol=0),
                                     np.isclose((eccs_for_scaffold[:,2]).astype(int), end_region, atol=tolerance, rtol=0))]
     if np.shape(ecc_matches)[0] > 0:
         regions_with_overlap.append(ecc_matches)
