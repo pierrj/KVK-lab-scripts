d1c2c8b3a398df266193d0d7b8e4ac97fe1495d1
Pierre Joubert <pierrj@berkeley.edu>
2021-06-10 11:21:11 -0700

    check hotspot as well

---
+++ python/check_hotspot_ecc_and_regions.py
@@ -0,0 +1,41 @@
+import csv
+import numpy as np
+import sys
+from statistics import median
+from statistics import mean
+
+regions_file = sys.argv[1]
+ecc_file = sys.argv[2]
+
+regions = []
+with open(regions_file, newline = '') as file:
+    file_reader = csv.reader(file, delimiter = '\t')
+    for row in file_reader:
+        regions.append([row[0], int(row[1]), int(row[2])])
+
+eccs = []
+with open(ecc_file, newline = '') as file:
+    file_reader = csv.reader(file, delimiter = '\t')
+    for row in file_reader:
+        eccs.append([row[0], int(row[1]), int(row[2])])
+
+
+eccs_indexed = [[] for i in range(56)]
+for ecc in eccs:
+    scaffold_num = int(ecc[0][10:12])-1
+    eccs_indexed[scaffold_num].append(ecc)
+eccs_arrays = []
+for i in range(len(eccs_indexed)):
+    eccs_arrays.append(np.array(eccs_indexed[i], dtype=object))
+
+ecc_count_per_region = []
+for region in regions:
+    start_region = region[1]
+    end_region= region[2]
+    eccs_for_scaffold = eccs_arrays[int(region[0][10:12])-1]
+    ecc_matches = eccs_for_scaffold[np.logical_or(np.logical_and(eccs_for_scaffold[:,1] >= start_region, eccs_for_scaffold[:,1] <= end_region)
+                                    ,np.logical_and(eccs_for_scaffold[:,2] >= start_region, eccs_for_scaffold[:,2] <= end_region))]
+    ecc_count_per_region.append(np.shape(ecc_matches)[0])
+
+print(median(ecc_count_per_region))
+print(mean(ecc_count_per_region))
