44ff9e9bc2cb624e6d6cc972f27f674660e346dd
Pierre Joubert <pierrj@berkeley.edu>
2021-09-20 12:41:23 -0700

    ecc caller v moller comparison for all samples

---
+++ bash/moller_sample_translation.sh
@@ -0,0 +1,35 @@
+
+
+echo -e SRR6315404'\t'01t >> mapfile_muscle_moller_samples
+echo -e SRR6315430'\t'10t >> mapfile_muscle_moller_samples
+echo -e SRR6315427'\t'11t >> mapfile_muscle_moller_samples
+echo -e SRR6315428'\t'12t >> mapfile_muscle_moller_samples
+echo -e SRR6315425'\t'13t >> mapfile_muscle_moller_samples
+echo -e SRR6315426'\t'14t >> mapfile_muscle_moller_samples
+echo -e SRR6315400'\t'15t >> mapfile_muscle_moller_samples
+echo -e SRR6315399'\t'16t >> mapfile_muscle_moller_samples
+echo -e SRR6315403'\t'02t >> mapfile_muscle_moller_samples
+echo -e SRR6315412'\t'03t >> mapfile_muscle_moller_samples
+echo -e SRR6315411'\t'04t >> mapfile_muscle_moller_samples
+echo -e SRR6315433'\t'05t >> mapfile_muscle_moller_samples
+echo -e SRR6315434'\t'06t >> mapfile_muscle_moller_samples
+echo -e SRR6315431'\t'07t >> mapfile_muscle_moller_samples
+echo -e SRR6315432'\t'08t >> mapfile_muscle_moller_samples
+echo -e SRR6315429'\t'09t >> mapfile_muscle_moller_samples
+
+echo -e SRR6315418'\t'01b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315421'\t'10b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315410'\t'11b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315409'\t'12b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315408'\t'13b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315407'\t'14b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315406'\t'15b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315405'\t'16b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315417'\t'02b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315420'\t'03b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315419'\t'04b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315414'\t'05b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315413'\t'06b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315416'\t'07b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315415'\t'08b >> mapfile_leukocytes_moller_samples
+echo -e SRR6315422'\t'09b >> mapfile_leukocytes_moller_samples
