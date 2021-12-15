ba481122bf0ce24ea1b26f772a42ebc441fdd6ac
Pierre Joubert <pierrj@berkeley.edu>
2021-12-15 21:23:44 +0100

    bases

--- bash/tblastn_validation.sh
+++ bash/tblastn_validation.sh
@@ -12,10 +12,11 @@ esac
 done
 
 genome_base=$(basename ${LOST_GENOME})
+og_base=$(basename ${LOST_OG})
 
 tblastn -query ${LOST_OG} -subject ${LOST_GENOME} \
     -max_intron_length 3000 \
     -outfmt "6 qacc sacc evalue qlen qstart qend sstart send  pident qcovs qcovhsp"  \
-    -max_target_seqs 1 > tblastn_${genome_base}_${LOST_OG}
+    -max_target_seqs 1 > tblastn_${genome_base}_${og_base}
 
 python /global/home/users/pierrj/git/python/parse_tblastn_hits.py tblastn_${LOST_GENOME}_${LOST_OG} ${E_VALUE} ${PIDENT} ${QUERY_COV} ${HIT_COUNT} ${LOST_OG}
