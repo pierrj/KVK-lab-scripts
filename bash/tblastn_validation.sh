275b014ae6c1ca75fbd1b9e0140cf12394babf83
Pierre Joubert <pierrj@berkeley.edu>
2021-12-15 21:26:24 +0100

    has to go to python too

--- bash/tblastn_validation.sh
+++ bash/tblastn_validation.sh
@@ -19,4 +19,4 @@ tblastn -query ${LOST_OG} -subject ${LOST_GENOME} \
     -outfmt "6 qacc sacc evalue qlen qstart qend sstart send  pident qcovs qcovhsp"  \
     -max_target_seqs 1 > tblastn_${genome_base}_${og_base}
 
-python /global/home/users/pierrj/git/python/parse_tblastn_hits.py tblastn_${LOST_GENOME}_${LOST_OG} ${E_VALUE} ${PIDENT} ${QUERY_COV} ${HIT_COUNT} ${LOST_OG}
+python /global/home/users/pierrj/git/python/parse_tblastn_hits.py tblastn_${genome_base}_${og_base} ${E_VALUE} ${PIDENT} ${QUERY_COV} ${HIT_COUNT} ${LOST_OG}
