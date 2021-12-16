50d7e6e86266ad146d2b9f058126d87a4c16804b
Pierre Joubert <pierrj@berkeley.edu>
2021-12-16 14:33:39 +0100

    lots of changes, mostly just dealing with grouping hsps within one sequence, also calculating qcov by hand now

--- bash/tblastn_validation.sh
+++ bash/tblastn_validation.sh
@@ -16,8 +16,7 @@ og_base=$(basename ${LOST_OG})
 
 tblastn -query ${LOST_OG} -subject ${LOST_GENOME} \
     -max_intron_length 3000 \
-    -outfmt "6 qacc sacc evalue qlen qstart qend sstart send  pident qcovs qcovhsp"  \
-    -max_hsps 1 \
+    -outfmt "6 qacc sacc evalue qlen qstart qend sstart send  pident"  \
     -max_target_seqs 1 > tblastn_${genome_base}_${og_base}
 
 python /global/home/users/pierrj/git/python/parse_tblastn_hits.py tblastn_${genome_base}_${og_base} ${E_VALUE} ${PIDENT} ${QUERY_COV} ${HIT_COUNT} ${LOST_OG}
--- python/parse_tblastn_hits.py
+++ python/parse_tblastn_hits.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import more_itertools as mit
+from itertools import filterfalse
 import sys
 
 input_file = sys.argv[1]
@@ -9,30 +11,65 @@ query_cov = int(sys.argv[4])
 hit_count = int(sys.argv[5])
 og = sys.argv[6]
 
-hits = {}
+prelim_hits = {}
 
 with open(input_file, newline = '') as file:
     file_reader = csv.reader(file, delimiter = '\t')
     for row in file_reader:
-        if row[0] not in hits:
-            hits[row[0]] = []
-        hits[row[0]].append([row[1],float(row[2]),int(row[3]),int(row[4]),
-                             int(row[5]),int(row[6]),int(row[7]),float(row[8]),int(row[9]),int(row[10])])
-hits_arrays = {}
+        if row[0] not in prelim_hits:
+            prelim_hits[row[0]] = []
+        prelim_hits[row[0]].append([row[1],float(row[2]),int(row[3]),int(row[4]),
+                             int(row[5]),int(row[6]),int(row[7]),float(row[8])])
+prelim_hits_arrays = {}
 
-for key in hits.keys():
-    hits_arrays[key] = np.array(hits[key], dtype=object)
+for key in prelim_hits.keys():
+    prelim_hits_arrays[key] = np.array(prelim_hits[key], dtype=object)
+
+parsed_hits = {}
+
+for protein in prelim_hits.keys():
+    prelim_hit_sorted = prelim_hits_arrays[protein][prelim_hits_arrays[protein][:, 5].argsort()]
+    count = 0
+    previous = 0
+    for index in range(len(prelim_hit_sorted)):
+        hsp = prelim_hit_sorted[index]
+        if np.any(previous != 0):
+            if hsp[5] - previous[6] < 3000:
+                parsed_hits[protein+'_'+str(count)].append(hsp)
+                previous = hsp
+            elif index == len(prelim_hit_sorted)-1:
+                count += 1
+                parsed_hits[protein+'_'+str(count)] = []
+                parsed_hits[protein+'_'+str(count)].append(hsp)
+            else:
+                count += 1
+                parsed_hits[protein+'_'+str(count)] = []
+                parsed_hits[protein+'_'+str(count)].append(hsp)
+                previous = hsp
+        else:
+            previous = hsp
+            parsed_hits[protein+'_'+str(count)] = []
+            parsed_hits[protein+'_'+str(count)].append(hsp)
+            
+parsed_hits_arrays = {}
+
+for key in parsed_hits.keys():
+    parsed_hits_arrays[key] = np.array(parsed_hits[key], dtype=object)
 
 valid_hits = []
 
-for protein in hits_arrays:
-    hit = hits_arrays[protein]
+for protein in parsed_hits_arrays:
+    hit = parsed_hits_arrays[protein]
     if (np.max(hit[:,0]) == np.min(hit[:,0]) and # all same scaffold
        np.all(hit[:,1] < e_value) and # e-value cutoff
-       np.all(hit[:,7] > pident) and # pident cutoff
-       np.all(hit[:,8] > query_cov) # query cov cutoff 
+       np.all(hit[:,7] > pident) # pident cutoff
        ):
-        valid_hits.append(protein)
+        protein_size_range = range(1,hit[0,2])
+        for i in hit:
+            protein_size_range = list(filterfalse(lambda x: i[3] <= x <= i[4], protein_size_range)) # get query cov
+        if (1-len(protein_size_range))*100 > query_cov: # check if query cov is enough
+            if protein[:-2] not in valid_hits: # same protein cant be counted twice for two alignments
+                valid_hits.append(protein)
 
 if len(valid_hits) >= hit_count:
     print(og + '\tyes')
@@ -40,31 +77,38 @@ else:
     print(og+'\tno')
 
 same_scaffold_filtered_hits = []
-for protein in hits_arrays:
-    hit = hits_arrays[protein]
+for protein in parsed_hits_arrays:
+    hit = parsed_hits_arrays[protein]
     if np.max(hit[:,0]) == np.min(hit[:,0]):
-        same_scaffold_filtered_hits.append(protein)
+        if protein[:-2] not in same_scaffold_filtered_hits:
+            same_scaffold_filtered_hits.append(protein[:-2])
 
 evalue_filtered_hits = []
-for protein in hits_arrays:
-    hit = hits_arrays[protein]
+for protein in parsed_hits_arrays:
+    hit = parsed_hits_arrays[protein]
     if np.all(hit[:,1] < e_value):
-        evalue_filtered_hits.append(protein)
+        if protein[:-2] not in evalue_filtered_hits:
+            evalue_filtered_hits.append(protein[:-2])
 
 pident_filtered_hits = []
-for protein in hits_arrays:
-    hit = hits_arrays[protein]
+for protein in parsed_hits_arrays:
+    hit = parsed_hits_arrays[protein]
     if np.all(hit[:,7] > pident):
-        pident_filtered_hits.append(protein)
+        if protein[:-2] not in pident_filtered_hits:
+            pident_filtered_hits.append(protein[:-2])
 
 query_cov_filtered_hits = []
-for protein in hits_arrays:
-    hit = hits_arrays[protein]
-    if np.all(hit[:,8] > query_cov):
-        query_cov_filtered_hits.append(protein)
+for protein in parsed_hits_arrays:
+    hit = parsed_hits_arrays[protein]
+    protein_size_range = range(1,hit[0,2])
+    for i in hit:
+        protein_size_range = list(filterfalse(lambda x: i[3] <= x <= i[4], protein_size_range)) # get query cov
+    if (1-len(protein_size_range))*100 > query_cov:
+        if protein[:-2] not in query_cov_filtered_hits:
+            query_cov_filtered_hits.append(protein[:-2])
 
 print('total')
-print(len(list(hits_arrays.keys())))
+print(len(list(parsed_hits_arrays.keys())))
 print('same scaffold filter')
 print(len(same_scaffold_filtered_hits))
 print('evalue filter')
