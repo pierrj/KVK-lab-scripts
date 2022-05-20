459539fae609d816f5045d27c005a5e0a69f0526
Pierre Joubert <pierrj@berkeley.edu>
2022-05-20 11:09:29 -0700

    try with scaling region to 0

--- bash/tblastn_validation_launch_script.sh
+++ bash/tblastn_validation_launch_script.sh
@@ -11,7 +11,7 @@ HIT_COUNT=2
 N_NODES=40
 OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_table
 BLAST_DB=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/all_ogs_seqs.fasta
-ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/absences_to_validate.tsv ## location of table of absences to validate
+ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/absences_to_validate.txt ## location of table of absences to validate
 
 
 if [ -f "jobqueue" ]; then
@@ -37,33 +37,38 @@ fi
 for node in $(seq -f "%02g" 1 ${N_NODES})
 do
     echo $node
-    sbatch -p savio --ntasks-per-node 20 --job-name=$node.tblastn_validation --export=node=$node,OUTPUT_FILE=$OUTPUT_FILE /global/home/users/pierrj/git/slurm/gnu_parallel_multinode.slurm
+    sbatch -p savio2 --ntasks-per-node 24 --job-name=$node.tblastn_validation --export=node=$node,OUTPUT_FILE=$OUTPUT_FILE /global/home/users/pierrj/git/slurm/gnu_parallel_multinode.slurm
 done
 
+## after everything is done
+
+awk -v OFS='\t' '{ if ($3 == "yes") {print $1, $2, $3} else {print $1, $2, "no"}}' ${OUTPUT_FILE} > ${OUTPUT_FILE}.simplified
 
 
 
+## ugh test old version...
 
+cd /global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_validation_old_version
 
-cd /global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/false_positive_rate_fungap_or_orthofinder/test_blast_id
-
-LOST_GENOME=/global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/CH0043.fasta
+LOST_GENOME_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/assemblies # where the assemblies are
+LOST_OG_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/orthofinder_4_27_2022/Results_Apr28/WorkingDirectory/OrthoFinder/Results_Apr28/Orthogroup_Sequences # where the OG_protein fastas are
 E_VALUE=1e-10
-HIT_COUNT=2
-QUERY_COV=55
 PIDENT=55
-BLAST_DB=/global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/false_positive_rate_fungap_or_orthofinder/all_proteins_with_og_names_no_CH0043.fasta
+QUERY_COV=55
+HIT_COUNT=2
 N_NODES=40
-OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/false_positive_rate_fungap_or_orthofinder/test_blast_id/optimization_out_5_9_22_wout_ch0043_in_blastdb
+OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_table_old_version
+ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/absences_to_validate.txt ## location of table of absences to validate
 
 if [ -f "jobqueue" ]; then
     rm jobqueue
 fi
 
-while read og; do
-    echo /global/home/users/pierrj/git/bash/tblastn_validation.sh -l /global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/false_positive_rate/orthogroup_protein_out_no_CH0043/${og}_protein.fasta \
-    -g ${LOST_GENOME} -e ${E_VALUE} -p ${PIDENT} -q ${QUERY_COV} -c ${HIT_COUNT} -d ${BLAST_DB} >> jobqueue
-done < /global/scratch/users/pierrj/PAV_SV/PAV/exonerate_validation/false_positive_rate/og_list_all_presences_CH0043
+
+while read -r LOST_GENOME LOST_OG; do
+    echo /global/home/users/pierrj/git/bash/tblastn_validation_dec_27_version.sh -l ${LOST_OG_DIR}/${LOST_OG}.fa -g ${LOST_GENOME_DIR}/${LOST_GENOME} \
+        -e ${E_VALUE} -p ${PIDENT} -q ${QUERY_COV} -c ${HIT_COUNT} >> jobqueue
+done < ${ABSENCES_FILE}
 
 mv jobqueue jobqueue_old
 
@@ -78,5 +83,5 @@ fi
 for node in $(seq -f "%02g" 1 ${N_NODES})
 do
     echo $node
-    sbatch -p savio --ntasks-per-node 20 --job-name=$node.tblastn_validation --export=node=$node,OUTPUT_FILE=$OUTPUT_FILE /global/home/users/pierrj/git/slurm/gnu_parallel_multinode.slurm
+    sbatch -p savio2 --ntasks-per-node 24 --job-name=$node.tblastn_validation --export=node=$node,OUTPUT_FILE=$OUTPUT_FILE /global/home/users/pierrj/git/slurm/gnu_parallel_multinode.slurm
 done
