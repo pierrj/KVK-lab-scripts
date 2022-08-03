8318c1464a58f66faa1785c15d3344e8d623be87
Pierre Joubert <pierrj@berkeley.edu>
2022-08-03 09:15:43 -0700

    testing this busco bug for the cluster people...

--- bash/fungap_launcher.sh
+++ bash/fungap_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /global/scratch/users/pierrj/fungap_runs/gladieux_all/
+cd /global/scratch/users/pierrj/fungap_runs/wheat_blast/
 
 while read genome; do
     if [ -d "${genome}" ]; then
@@ -22,4 +22,17 @@ while read genome; do
 done < genomes_mapfile
 
 
-sbatch -p savio3 --ntasks-per-node=32 --job-name=${genome}_run_fungap --export=genome=$genome /global/home/users/pierrj/git/slurm/run_fungap.slurm
+sbatch -p savio3 --ntasks-per-node=32 --job-name=${genome}_run_fungap --export=genome=$genome /global/home/users/pierrj/git/slurm/run_fungap.slurm\
+
+## to relaunch failed jobs due to busco download error
+
+squeue -u pierrj --format="%.100j" | tail -n +2 | awk '{print substr($1, 0,length($1)-11)}' > running_jobs
+
+while read genome; do
+    if grep -Fxq "$genome" running_jobs
+    then
+        echo "$genome is already running"
+    else
+    sbatch -p savio3 --ntasks-per-node=32 --job-name=${genome}_run_fungap --export=genome=$genome /global/home/users/pierrj/git/slurm/run_fungap.slurm
+    fi
+done < genomes_mapfile
