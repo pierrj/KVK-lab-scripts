1a5a58ef4ad2d598a760d5818b9aa2771427d011
Pierre Joubert <pierrj@berkeley.edu>
2022-08-06 11:04:47 -0700

    try a grid search on the smote adjusted data

--- bash/tblastn_validation_launch_script.sh
+++ bash/tblastn_validation_launch_script.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-cd /global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_validation
+cd /global/scratch/users/pierrj/PAV_SV/PAV/wheat_blast/pav_validation
 
-LOST_GENOME_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/assemblies # where the assemblies are
-LOST_OG_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/orthofinder_4_27_2022/Results_Apr28/WorkingDirectory/OrthoFinder/Results_Apr28/Orthogroup_Sequences # where the OG_protein fastas are
+LOST_GENOME_DIR=/global/scratch/users/pierrj/fungap_runs/wheat_blast/genomes_to_annotate # where the assemblies are
+LOST_OG_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/wheat_blast/orthofinder/Results_Aug05/WorkingDirectory/OrthoFinder/Results_Aug05/Orthogroup_Sequences # where the OG_protein fastas are
 E_VALUE=1e-10
 PIDENT=55
 QUERY_COV=55
 HIT_COUNT=2
 N_NODES=40
-OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_table
-BLAST_DB=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/all_ogs_seqs.fasta
-ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/absences_to_validate.txt ## location of table of absences to validate
+OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/wheat_blast/pav_table
+BLAST_DB=/global/scratch/users/pierrj/PAV_SV/PAV/wheat_blast/all_ogs_seqs.fasta
+ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/wheat_blast/absences_to_validate.tsv ## location of table of absences to validate
 
+conda activate /global/scratch/users/pierrj/conda_envs/orthofinder
+
+/global/scratch/users/pierrj/conda_envs/orthofinder/bin/python /global/home/users/pierrj/git/python/make_single_file_from_og_dir.py ${LOST_OG_DIR} ${BLAST_DB}
+
+makeblastdb -in ${BLAST_DB} -dbtype prot
+
+tr -d '\015' <${ABSENCES_FILE} > ${ABSENCES_FILE}.fixed
 
 if [ -f "jobqueue" ]; then
     rm jobqueue
 fi
 
-
 while read -r LOST_GENOME LOST_OG; do
     echo /global/home/users/pierrj/git/bash/tblastn_validation.sh -l ${LOST_OG_DIR}/${LOST_OG}.fa -g ${LOST_GENOME_DIR}/${LOST_GENOME} \
         -e ${E_VALUE} -p ${PIDENT} -q ${QUERY_COV} -c ${HIT_COUNT} -d ${BLAST_DB} >> jobqueue
-done < ${ABSENCES_FILE}
+done < ${ABSENCES_FILE}.fixed
 
 mv jobqueue jobqueue_old
 
@@ -43,45 +49,3 @@ done
 ## after everything is done
 
 awk -v OFS='\t' '{ if ($3 == "yes") {print $1, $2, $3} else {print $1, $2, "no"}}' ${OUTPUT_FILE} > ${OUTPUT_FILE}.simplified
-
-
-
-## ugh test old version...
-
-cd /global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_validation_old_version
-
-LOST_GENOME_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/assemblies # where the assemblies are
-LOST_OG_DIR=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/orthofinder_4_27_2022/Results_Apr28/WorkingDirectory/OrthoFinder/Results_Apr28/Orthogroup_Sequences # where the OG_protein fastas are
-E_VALUE=1e-10
-PIDENT=55
-QUERY_COV=55
-HIT_COUNT=2
-N_NODES=40
-OUTPUT_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/pav_table_old_version
-ABSENCES_FILE=/global/scratch/users/pierrj/PAV_SV/PAV/re_gladieux_proteomes/absences_to_validate.txt ## location of table of absences to validate
-
-if [ -f "jobqueue" ]; then
-    rm jobqueue
-fi
-
-
-while read -r LOST_GENOME LOST_OG; do
-    echo /global/home/users/pierrj/git/bash/tblastn_validation_dec_27_version.sh -l ${LOST_OG_DIR}/${LOST_OG}.fa -g ${LOST_GENOME_DIR}/${LOST_GENOME} \
-        -e ${E_VALUE} -p ${PIDENT} -q ${QUERY_COV} -c ${HIT_COUNT} >> jobqueue
-done < ${ABSENCES_FILE}
-
-mv jobqueue jobqueue_old
-
-shuf jobqueue_old > jobqueue
-
-split --number=l/${N_NODES} --numeric-suffixes=1 jobqueue jobqueue_
-
-if [ -f "${OUTPUT_FILE}" ]; then
-    rm ${OUTPUT_FILE}
-fi
-
-for node in $(seq -f "%02g" 1 ${N_NODES})
-do
-    echo $node
-    sbatch -p savio2 --ntasks-per-node 24 --job-name=$node.tblastn_validation --export=node=$node,OUTPUT_FILE=$OUTPUT_FILE /global/home/users/pierrj/git/slurm/gnu_parallel_multinode.slurm
-done
---
+++ python/grid_search_smote.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from statistics import mean
+from matplotlib import pyplot
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+
+df_genes = pd.read_csv('gene_info_w_proportions.csv')
+
+# remove lineage 4
+df_genes = df_genes[df_genes['lineage']!=4]
+
+df_genes = df_genes.drop(['id', 'scaffold', 'start', 'end', 'orientation', 'orthogroups', 'enough_space_te', 'enough_space_gene',
+                        'genome', 'lineage', 'lineage_conserved', 'proportion'], axis=1)
+
+y = df_genes['lineage_pav']
+X = df_genes.drop('lineage_pav', axis=1)
+
+#Use SMOTE to oversample the minority class
+oversample = SMOTE()
+over_X, over_y = oversample.fit_resample(X, y)
+over_X_train, over_X_test, over_y_train, over_y_test = train_test_split(over_X, over_y, test_size=0.1, stratify=over_y)
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 100, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestClassifier()
+# Random search of parameters, using 3 fold cross validation, 
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# Fit the random search model
+rf_random.fit(over_X_train, over_y_train)
+
+print(rf_random.best_params_)
