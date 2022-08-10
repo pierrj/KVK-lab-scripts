0a215c7a4eb01e7683c79055f2339a7d6e6f50fe
Pierre Joubert <pierrj@berkeley.edu>
2022-08-09 17:03:20 -0700

    almost there..

--- python/process_gffs_for_orthofinder.py
+++ python/process_gffs_for_orthofinder.py
@@ -20,7 +20,7 @@ for genome in genomes:
     print(genome)
     in_file = join(genome, 'fungap_out','fungap_out', 'fungap_out.nocomments.gff3')
     in_handle = open(in_file)
-    out_file = join(out_dir,genome+"_fungap_out.fixed.gff3")
+    out_file = join(out_dir,genome+"fungap_out.fixed.gff3")
     with open(out_file, "w") as out_handle:
         for rec in GFF.parse(in_handle):
             for feature in rec.features:
@@ -38,6 +38,7 @@ for genome in genomes:
                         for sub_feature in feature.sub_features[0].sub_features:
                             sub_feature.id = [original_feature_id + '_' + accession+'T0']
                             sub_feature.qualifiers['Parent'] = [original_feature_id + '_' + accession+'T0']
+                            sub_feature.qualifiers['ID'] = [original_feature_id + '_' + accession+'T0']
                     except KeyError:
                         print(feature)
             GFF.write([rec], out_handle)
