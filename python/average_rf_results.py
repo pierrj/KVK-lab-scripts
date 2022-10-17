b3cc40d647302441f7f96bdd51f362dfd4454474
Pierre Joubert <pierrj@berkeley.edu>
2022-10-17 12:22:01 -0700

    average with more params now

--- python/average_rf_results.py
+++ python/average_rf_results.py
@@ -5,8 +5,16 @@ input_file = sys.argv[1]
 output_name = sys.argv[2]
 
 df = pd.read_csv(input_file, sep='\t', header = None)
-df.columns = ['approach', 'majority_fraction', 'recall', 'precision', 'ap', 'auc']
+df.columns = ['approach', 'majority_fraction', 
+            'n_estimators', 'min_samples_split',
+            'min_samples_leaf', 'max_features',
+            'max_depth', 'bootstrap',
+            'recall', 'precision', 'ap', 'auc']
 
-df_grouped = df.groupby(["approach", "majority_fraction"]).mean().reset_index()
+
+df_grouped = df.groupby(['approach', 'majority_fraction', 
+            'n_estimators', 'min_samples_split',
+            'min_samples_leaf', 'max_features',
+            'max_depth', 'bootstrap']).mean().reset_index()
 
 df_grouped.to_csv(output_name,sep='\t', index=False)
